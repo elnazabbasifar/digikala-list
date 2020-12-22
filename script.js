16c7bf787e420f225943eeaf70e3e4a12912ddf2
farxp <nicki_20002000@yahoo.com>
2020-12-22 11:16:58 +0330

    add files

---
+++ script.js
@@ -0,0 +1,30 @@
+function submitHandler(){
+    var url = document.getElementById("input_url").value;
+    const expression = "^((http|https):\/\/|(http|https):\/\/?(www\.)|[wW]{3}\.|)digikala\.com\/product?(\/.*)?$"
+    const regex = new RegExp(expression);
+    
+    if (url.match(regex)) {     
+        
+        // Create anchor element. 
+        var a = document.createElement("a");                  
+        // Create the text node for anchor element. 
+        var link = document.createTextNode("This is link");        
+        // Append the text node to anchor element. 
+        a.appendChild(link);       
+        // Set the title. 
+        a.title = "This is Link";         
+        // Set the href property. 
+        a.href = String(url);         
+        // Create list element
+        var li = document.createElement("li"); 
+        // Append the anchor element to the list. 
+        li.innerHTML = a;
+        document.getElementsById("list").appendChild(li);
+        
+    }else{
+        alert("Type a correct url");
+    }
+    
+}
+
+
