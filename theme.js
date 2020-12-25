65fb94f4f2a54b3c47195955fd1991be2166b9dd
farxp <nicki_20002000@yahoo.com>
2020-12-26 01:52:46 +0330

    fix errors

--- theme.js
+++ theme.js
@@ -3,73 +3,50 @@ function modeHandler(){
     element.classList.toggle("dark-mode");
 }
 
-function getColorofBackImage(){
-    const colorThief = new ColorThief();
-    const img = document.querySelector('img');
-    console.log(img);
-    img.crossOrigin = 'Anonymous';
-    // Make sure image is finished loading
-    if (img.complete) {
-        return colorThief.getColor(img);
-    } else {
-        img.addEventListener('load', function() {
-            return colorThief.getColor(img);
-        });
+//============================================================
+// Auto select dark or light theme based on the dominant color of the background image.
+function setTheme(){
+  
+  const colorThief = new ColorThief();
+  const img = document.querySelector('img');
+  img.crossOrigin = 'Anonymous';
+
+  img.addEventListener('load', function() {
+
+    // Grab the dominant color of an image.
+    dominantColor = colorThief.getColor(img);
+
+    // Call lightOrDark function to get the brightness (light or dark)
+    brightness = lightOrDark(dominantColor);
+        
+    // If the background color is dark, add the light-text class to it
+    if(brightness == 'dark') {
+      document.body.classList.add('dark-mode');
+        // element.classList.add('light-text');
+    }else {
+      document.body.classList.add('light-mode');
     }
-}
+  });
 
-function lightOrDark(color) {
+}//func
+//============================================================
+//calculates the color’s HSP value to determine whether the color is light or dark
+function lightOrDark(rgbColor) {
 
-  // Check the format of the color, HEX or RGB?
-  if (color.match(/^rgb/)) {
-
-    // If HEX --> store the red, green, blue values in separate variables
-    color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
-
-    r = color[1];
-    g = color[2];
-    b = color[3];
-  } 
-  else {
-
-    // If RGB --> Convert it to HEX: http://gist.github.com/983661
-    color = +("0x" + color.slice(1).replace( 
-      color.length < 5 && /./g, '$&$&'
-    )
-             );
-
-    r = color >> 16;
-    g = color >> 8 & 255;
-    b = color & 255;
-  }
-
+  //store the red, green, blue values in separate variables
+  [r,g,b]=rgbColor;
+  
   // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
-  hsp = Math.sqrt(
+  var hsp = Math.sqrt(
     0.299 * (r * r) +
     0.587 * (g * g) +
     0.114 * (b * b)
   );
 
   // Using the HSP value, determine whether the color is light or dark
-  if (hsp>127.5) {
+  if (hsp>127.5)  return 'light';
 
-    return 'light';
-  } 
-  else {
-
-    return 'dark';
-  }
+  else            return 'dark';
 }
 
-colorofImage = getColorofBackImage();
-console.log(colorofImage);
-brightness = lightOrDark(colorofImage);
-  
-// If the background color is dark, add the light-text class to it
-if(brightness == 'dark') {
-    modeHandler();
-    // element.classList.add('light-text');
-}
-// else {
-//     element.classList.add('dark-text');
-// }
+setTheme();
