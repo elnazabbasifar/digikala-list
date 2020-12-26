ef8e2b1fbf8689787c057cae4ea1afb51ebdb3aa
farxp <nicki_20002000@yahoo.com>
2020-12-26 03:59:44 +0330

    add onload event & a line to call setTheme()

--- axios.js
+++ axios.js
@@ -1,18 +1,20 @@
 // get Bing's today url from Flask app to set it as background.
-function setBackground(path) { 
-    //make Get Request
-    axios.get(path).then( 
-        (response) => { 
-            console.log(response); 
-            var imageUrl = response.data;
-            console.log(imageUrl);
-            // Set a background image for a document.
-            document.body.style.backgroundImage = "url("+imageUrl+")";
-        }, 
-        (error) => { 
-            console.log(error); 
-        } 
-    ); 
-} 
+function setBackground(path) {
+    document.body.onload = function () { 
+        //make Get Request
+        axios.get(path).then( 
+            (response) => { 
+                var imageUrl = response.data;
+                console.log(imageUrl);
+                // Set a background image for a document.
+                document.body.style.backgroundImage = "url("+imageUrl+")";
+                setTheme(imageUrl); //located in theme.js
+            }, 
+            (error) => { 
+                console.log(error); 
+            } 
+        ); 
+    } 
+}
 var url = "http://127.0.0.1:5000/bing";
 setBackground(url); 
